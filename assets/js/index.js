80624845f8c1a9ce5b700cecf18d0b38428c8e6b
muhammad almashal <muhammad.almashal@kuwaitnet.email>
2025-08-05 16:38:02 +0300

    Edit

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,13 +3,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('nav .popup-menu')
     const toggleMenu = document.querySelector('nav .options .toggle-menu')
     const toggleInnerList = document.querySelectorAll('nav .popup-menu .list-menu li .toggle-inner-list')
+    
     toggleMenu.addEventListener('click', () => {
-        menu.classList.add('show')
+        menu.classList.toggle('show')
     })
     toggleInnerList.forEach((btn) => {
         btn.addEventListener('click', () => {
-            toggleInnerList.forEach((e) => e.classList.remove('show'))
-            btn.classList.add('show')
+            if (btn.classList.contains('show')) {
+                btn.classList.toggle('show')
+
+            }
+            else {
+                toggleInnerList.forEach((e) => e.classList.remove('show'))
+                btn.classList.toggle('show')
+            }
         })
     })
+
+
+    const currentYear = document.querySelector('footer .current-year')
+    currentYear.innerHTML=new Date(Date.now()).getFullYear()
 })
