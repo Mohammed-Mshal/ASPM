e70480ea22fa054e00d8ae8516a744703d0e0481
Mohammed-Mshal <83920083+Mohammed-Mshal@users.noreply.github.com>
2025-08-06 13:13:42 +0300

    Edit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    document.addEventListener('scroll',()=>{
+        window.scrollY>=50?
+            document.querySelector('nav').classList.add('scrolling')
+            :
+            document.querySelector('nav').classList.remove('scrolling')
+        
+    })
     const menu = document.querySelector('nav .popup-menu')
     const toggleMenu = document.querySelector('nav .options .toggle-menu')
     const toggleInnerList = document.querySelectorAll('nav .popup-menu .list-menu li .toggle-inner-list')
     
     toggleMenu.addEventListener('click', () => {
-        menu.classList.toggle('show')
-    })
+        menu.classList.toggle('show');
+        const nav = document.querySelector('nav');
+        const containerPopup = menu.querySelector('.container-popup');
+        function handleClickOutside(e) {
+            if (e.target.closest('.popup-menu')&&!e.target.closest('.container-popup') && !e.target.closest('.toggle-menu')) {
+                menu.classList.remove('show');
+                document.removeEventListener('click', handleClickOutside);
+            }
+        }
+        setTimeout(() => {
+            document.addEventListener('click', handleClickOutside);
+        }, 0);
+    });
     toggleInnerList.forEach((btn) => {
         btn.addEventListener('click', () => {
             if (btn.classList.contains('show')) {
                 btn.classList.toggle('show')
-
             }
             else {
                 toggleInnerList.forEach((e) => e.classList.remove('show'))
